4cbfba10c2d95d3636d322b6354ff0ddfcaa65a8
nolsen <nolsen@regis.edu>
2013-09-11 07:48:58 -0600

    V1 of Test

---
+++ EFCrudTest1/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFCrudTest1.Models
+{
+    public class Contact
+    {
+        [Required]
+        public Int32 ID { get; set; }
+        [Required]
+        public String FirstName { get; set; }
+        public String MiddleName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State {get; set; }
+        public String Zip {get; set;}
+        public String Country {get; set;}
+    }
+
+    public class DBContact : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ EFCrudTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EFCrudTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("EFCrudTest1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b89714c-0ffe-42a6-bd37-e0727cff3efd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
